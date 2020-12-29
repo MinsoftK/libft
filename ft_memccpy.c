0425c69587972ed5cc54a7b0c74dd536ab15f6e7
minsungk <minsungk@student.42seoul.kr>
2020-12-29 23:35:08 +0900

    ft_memccpy Norm check

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,7 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsungk <minsungk@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/29 22:08:56 by minsungk          #+#    #+#             */
+/*   Updated: 2020/12/29 22:08:57 by minsungk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h";
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t num)
 {
+	size_t i;
 
+	i = 0;
+	if (!dest)
+		return (0);
+	if (!num || dest == src)
+		return (dest);
+	while (i++ < num)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		if ((unsigned char)c == ((unsigned char *)src)[i])
+			break ;
+	}
+	if (num == i)
+		return ((void *)0);
+	return (dest + i + 1);
 }
