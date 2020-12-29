aa812f458b12a2a9660f46409adf6980e467daf6
minsungk <minsungk@student.42seoul.kr>
2020-12-29 23:34:39 +0900

    ft_memcmp NormCheck

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsungk <minsungk@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/29 23:17:37 by minsungk          #+#    #+#             */
+/*   Updated: 2020/12/29 23:17:38 by minsungk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		*ft_memcmp(const void *ptr1, const void *ptr2, size_t num)
+{
+	size_t			i;
+	unsigned char	*temp1;
+	unsigned char	*temp2;
+
+	i = 0;
+	temp1 = (unsigned char *)ptr1;
+	temp2 = (unsigned char *)ptr2;
+	while (i++ < num)
+	{
+		if (temp1[i] != temp2[i])
+			return (temp1[i] - temp2[i]);
+	}
+}
