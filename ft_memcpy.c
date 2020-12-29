ad751868b8c6a1a64a9aef097ed0af153cfd3a2a
minsungk <minsungk@student.42seoul.kr>
2020-12-29 23:34:29 +0900

    ft_memcpy NormCheck

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,5 +14,14 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t num)
 {
+	size_t i;
 
+	i = 0;
+	if (!dest)
+		return (0);
+	if (!num || dest == src)
+		return (dest);
+	while (i++ < num)
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+	return (dest);
 }
