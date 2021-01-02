9f810fc888db62c7a8a5715df98eea86039880e8
minsungk <minsungk@student.42seoul.kr>
2021-01-03 02:37:25 +0900

    fix ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,11 +17,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t num)
 	size_t i;
 
 	i = 0;
-	if (!dest)
-		return (0);
-	if (!num || dest == src)
-		return (dest);
-	while (i++ < num)
+	if (!src || !num)
+		return ((void *)0);
+	while (i < num)
+	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
 	return (dest);
 }
