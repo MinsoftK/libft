395d1fc76b90138a6161b35a4044436be34a54a3
minsungk <minsungk@student.42seoul.kr>
2021-01-03 02:17:18 +0900

    fix memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -17,10 +17,11 @@ void	*ft_memchr(const void *ptr, int value, size_t num)
 	size_t	i;
 
 	i = 0;
-	while (i++ < num)
+	while (i < num)
 	{
 		if (((unsigned char *)ptr)[i] == (unsigned char)value)
 			return ((void *)ptr + i);
+		i++;
 	}
 	return ((void *)0);
 }
