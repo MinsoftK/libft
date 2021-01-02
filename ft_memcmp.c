9e7c77a22331084669a857570f898cc97e014e77
minsungk <minsungk@student.42seoul.kr>
2021-01-03 02:32:35 +0900

    fix ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,10 +21,11 @@ int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num)
 	i = 0;
 	temp1 = (unsigned char *)ptr1;
 	temp2 = (unsigned char *)ptr2;
-	while (i++ < num)
+	while (i < num)
 	{
 		if (temp1[i] != temp2[i])
 			return (temp1[i] - temp2[i]);
+		i++;
 	}
 	return (0);
 }
