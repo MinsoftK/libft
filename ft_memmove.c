ab9bcd2f56bebf7400787dc6f324d56426e2a909
minsungk <minsungk@student.42seoul.kr>
2021-01-02 19:44:11 +0900

    fix return value

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsungk <minsungk@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/30 18:31:14 by minsungk          #+#    #+#             */
-/*   Updated: 2020/12/30 18:31:15 by minsungk         ###   ########.fr       */
+/*   Created: 2020/12/31 23:17:10 by minsungk          #+#    #+#             */
+/*   Updated: 2020/12/31 23:17:12 by minsungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	size_t temp;
-
-	i = 0;
-	while (big[i] && i < len)
-	{
-		temp = 0;
-		if (little[temp] == big[i + temp])
-		{
-			while (little[temp] && big[i + temp])
-			{
-				if (little[temp] != big[i + temp] || (i + temp) >= len)
-					break ;
-				temp++;
-			}
-			if (little[temp] == '\0')
-				return (&((char *)big)[i]);
-		}
-		i++;
-	}
-	return ((void *)0);
+	
 }
--- libft.h
+++ libft.h
@@ -16,11 +16,12 @@
 # include <unistd.h>
 
 void				*ft_memset(void *dest, int value, size_t len);
-void				*ft_bzero(void *ptr, size_t len);
+void				ft_bzero(void *ptr, size_t len);
 void				*ft_memcpy(void *dest, const void *src, size_t num);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t num);
+void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *ptr, int value, size_t num);
-int					*ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
+int					ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 size_t				ft_strlen(const char *str);
 size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 size_t				ft_strlcat(char *dest, const char *src, size_t destsize);
@@ -29,6 +30,7 @@ char				*ft_strnstr(const char *big, const char *little
 , size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
+/*
 void				*ft_isalpha();
 void				*ft_isdigit();
 void				*ft_isascii();
@@ -67,5 +69,5 @@ void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *)
 , void (*del)(void *));
-
+*/
 #endif
