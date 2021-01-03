8f041d38385a8437d8660f2998b20ac30ede1dae
minsungk <minsungk@student.42seoul.kr>
2021-01-03 21:13:45 +0900

    fix Norm & grama

--- ft_split.c
+++ ft_split.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsungk <minsungk@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/03 19:42:40 by minsungk          #+#    #+#             */
-/*   Updated: 2021/01/03 19:42:55 by minsungk         ###   ########.fr       */
+/*   Created: 2021/01/03 19:43:29 by minsungk          #+#    #+#             */
+/*   Updated: 2021/01/03 19:43:30 by minsungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*temp;
-	size_t	i;
-
-	i = 0;
-	if (!(temp = (char *)malloc(len + 1)))
-		return (NULL);
-	while (i < len && s[start + i])
-	{
-		temp[i] = s[start + i];
-		i++;
-	}
-	temp[i] = '\0';
-	return (temp);
-}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsungk <minsungk@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/03 19:42:40 by minsungk          #+#    #+#             */
-/*   Updated: 2021/01/03 19:42:55 by minsungk         ###   ########.fr       */
+/*   Created: 2021/01/03 19:43:49 by minsungk          #+#    #+#             */
+/*   Updated: 2021/01/03 19:43:50 by minsungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*temp;
-	size_t	i;
-
-	i = 0;
-	if (!(temp = (char *)malloc(len + 1)))
-		return (NULL);
-	while (i < len && s[start + i])
-	{
-		temp[i] = s[start + i];
-		i++;
-	}
-	temp[i] = '\0';
-	return (temp);
-}
