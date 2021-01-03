f38f14ed2c9c46d5e88f0f0831f62b57bd02c3a7
minsungk <minsungk@student.42seoul.kr>
2021-01-04 06:27:59 +0900

    fix Norm, Makefile and header

--- libft.h
+++ libft.h
@@ -41,7 +41,6 @@ int					ft_toupper(int c);
 int					ft_tolower(int c);
 void				*ft_calloc(size_t count, size_t size);
 void				*ft_strdup(const char *s);
-
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
