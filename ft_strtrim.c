11e1fa3f2d571375f3626fe34f671ab5c862ba52
minsungk <minsungk@student.42seoul.kr>
2021-01-03 23:26:40 +0900

    add ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*trimStr(char *temp, char const *set);
+
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t 	i;
+	size_t 	len;
+	char	*temp;
+	
+	i = 0;
+	len = ft_strlen(s1);
+	if	(s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return (s1);
+	if (!(temp = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (s1[i])
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	temp[i] = '\0';
+	trimStr(temp, set);
+	return (temp);
+}
+
+void	*trimStr(char *temp, char const *set)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	while (temp[i])
+	{
+		j = 0;
+		if (temp[i] == set[j])
+		{
+			while (temp[i + j])
+			{
+				temp[i + j] = temp[i + j + 1];
+				j++;
+			}
+		}
+		i++;
+	}
+}
--- libft.h
+++ libft.h
@@ -42,7 +42,6 @@ int					ft_tolower(int c);
 void				*ft_calloc(size_t count, size_t size);
 void				*ft_strdup(const char *s);
 
-
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
@@ -54,7 +53,6 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-
 
 /*
 typedef struct		s_list
