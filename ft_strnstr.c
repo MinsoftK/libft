ad43faaab6e211dd6e023514ce01ab32ee1e2dc7
minsungk <minsungk@student.42seoul.kr>
2021-01-06 21:15:12 +0900

    test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,6 +18,8 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t temp;
 
 	i = 0;
+	 if (big == NULL || little == NULL)
+        return (NULL);
 	if (little[i] == '\0')
 		return ((char *)big);
 	while (big[i] && i < len)
