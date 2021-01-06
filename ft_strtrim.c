22b3e88ca635b5964746e16c8d00a8cf6a59e6e7
minsungk <minsungk@student.42seoul.kr>
2021-01-06 20:15:29 +0900

    fix *set == 0

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -21,7 +21,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	if (*s1 == 0 || *set == 0)
+	if (*s1 == 0)
 		return (0);
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
