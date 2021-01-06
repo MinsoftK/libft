fde94cff5d33751771b4dfebe859f4bd04a20474
minsungk <minsungk@student.42seoul.kr>
2021-01-06 19:53:57 +0900

    test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -27,6 +27,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
 	end = ft_strlen(s1);
+	if (i == end)
+		return (NULL);
 	while (end && ft_strchr(set, s1[end]))
 		end--;
 	if (!(temp = (char *)malloc(sizeof(char) * (end - i + 2))))
