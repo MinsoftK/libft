603b823020ba93a26641921a78a2203393ad0839
minsungk <minsungk@student.42seoul.kr>
2021-01-08 04:14:16 +0900

    test

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -11,7 +11,14 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	if (lst == NULL)
+		return ;
+	while (lst->next)
+	{
+		(*f)(lst);
+		lst = lst->next;
+	}
 }
