ff5bc6f4b53ed10ca27bf7f0028708167c0833ad
minsungk <minsungk@student.42seoul.kr>
2021-01-08 06:17:14 +0900

    test

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -15,18 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *temp;
+	t_list *start;
 	t_list *after_f;
 
 	temp = NULL;
+	start = lst;
 	if (!lst && !f)
 		return (NULL);
-	while (lst)
+	while (start)
 	{
 		after_f = ft_lstnew((*f)(lst->content));
 		if (!after_f)
 			break ;
 		ft_lstadd_back(&temp, after_f);
-		lst = lst->next;
+		start = lst->next;
 	}
 	if (ft_lstsize(lst) != ft_lstsize(temp))
 		ft_lstclear(&temp, del);
