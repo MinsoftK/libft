2274775bfe91ec4f2f6b309035346bdbd9561751
minsungk <minsungk@student.42seoul.kr>
2021-01-07 22:24:48 +0900

    ft_putnbr_fd Norm check

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,5 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, ft_itoa(n), 4);
+	long	temp;
+	int		i;
+	char	num[15];
+
+	i = 0;
+	if (n > 0)
+		temp = (long)n;
+	else if (n == 0)
+		write(fd, "0", 1);
+	else
+	{
+		temp = (long)n;
+		temp = -temp;
+	}
+	while (temp)
+	{
+		num[i++] = ((temp % 10) + '0');
+		temp /= 10;
+	}
+	if (n < 0)
+		write(fd, "-", 1);
+	while (--i >= 0)
+		write(fd, num + i, 1);
 }
