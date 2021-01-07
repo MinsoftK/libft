36c7331fcafb18993e17bdb5e4b860e7a77f5475
minsungk <minsungk@student.42seoul.kr>
2021-01-08 05:10:21 +0900

    ft_lst Norm Check

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -14,5 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list *temp;
+	t_list *after_f;
+
+	temp = NULL;
+	if (!lst && !f)
+		return (NULL);
+	while (lst)
+	{
+		after_f = ft_lstnew((*f)(lst->content));
+		if (!temp)
+			break ;
+		ft_lstadd_back(&temp, after_f);
+		lst = lst->next;
+	}
+	if (ft_lstsize(lst) != ft_lstsize(temp))
+		ft_lstclear(&temp, del);
+	return (temp);
 }
