8185672b8181f444fb5d457912d7c45d7e9d59f1
minsungk <minsungk@student.42seoul.kr>
2021-01-08 00:36:35 +0900

    ft_lstsize Norm check

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int		ft_lstsize(t_list *lst)
+{
+	size_t i;
+
+	i = 0;
+	if (!lst)
+		return (0);
+	while (lst->next)
+		i++;
+}
