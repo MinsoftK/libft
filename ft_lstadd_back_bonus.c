989914f8c79957e0a7bb73de0d2874f5c926b011
minsungk <minsungk@student.42seoul.kr>
2021-01-08 02:59:18 +0900

    test

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_lstnew_bonus.c"
-#include <string.h>
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
