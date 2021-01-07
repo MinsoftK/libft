b15ad73df165b50bbaad4d67e5556a59999a124d
minsungk <minsungk@student.42seoul.kr>
2021-01-08 00:31:59 +0900

    ft_lstadd_front Norm check

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!new || !lst)
+		return ;
+	if (*lst)
+		new->next = *lst;
+	else
+		new->next = NULL;
+	*lst = new;
+}
