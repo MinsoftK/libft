cb352d0d576f1b0b4ae1023701239bbfc1502c0d
minsungk <minsungk@student.42seoul.kr>
2021-01-08 05:59:03 +0900

    fix error

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -14,9 +14,12 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	if (lst == NULL)
+		return ;
 	while (lst->next)
 	{
 		(*f)(lst->content);
 		lst = lst->next;
 	}
+	(*f)(lst->content);
 }
