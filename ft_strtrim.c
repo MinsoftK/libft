f7d8dc513c8a88c232cfd134d1358c1f93f8f058
minsungk <minsungk@student.42seoul.kr>
2021-01-09 03:58:06 +0900

    test ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,24 +12,37 @@
 
 #include "libft.h"
 
+#include <stdio.h>
+#include "ft_strlen.c"
+#include "ft_strchr.c"
+#include "ft_strdup.c"
+#include "ft_substr.c"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t			i;
+	size_t			start;
 	size_t			end;
+	size_t			len;
 	char			*temp;
 
-	i = 0;
+	start = 0;
 	if (s1 == NULL)
 		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
-	while (s1[i] && ft_strchr(set, s1[i]))
-		i++;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
 	if (i == ft_strlen(s1))
 		return (ft_strdup(""));
 	end = ft_strlen(s1);
 	while (end && ft_strchr(set, s1[end - 1]))
 		end--;
-	temp = ft_substr((char *)s1, i, end - i + 2);
+	if (!(temp = (char *)malloc(sizeof(char) * (end - start + 2))))
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		temp[i++] = s1[start++];
+	temp[i] = '\0';
 	return (temp);
 }
